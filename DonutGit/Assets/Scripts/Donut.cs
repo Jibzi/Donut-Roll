798807dbae29e0843fdceadb2327d892412a8993
BrujoCervino <39952594+BrujoCervino@users.noreply.github.com>
2018-06-06 14:55:20 +0100

    I have made the functionality to move from side to side by pressing arrowkeys, though I still need a speed parameter to edit this.

--- DonutGit/Assets/Scripts/Donut.cs
+++ DonutGit/Assets/Scripts/Donut.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class Donut : MonoBehaviour
 {
+    private float horizontalMovement;
+    private Vector3 SpinSpeed;
+    //The points between which the donut's left-to-right movement is constrained
+    public float 
+        leftConstraint,
+        rightConstraint;
+    private Vector2 HorizontalConstraints;
+    private bool donutNeedsToStop;
 
-	private Vector3 SpinSpeed = new Vector3(0,0,0);
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Use this for initialization
+    void Start()
+    {
+        HorizontalConstraints = new Vector2(leftConstraint, rightConstraint);
+        SpinSpeed = new Vector3(0, 0, 0);
+        horizontalMovement = Input.GetAxis("Horizontal");
+    }
+
+    void ClampHorizontalMovement()
+    {
+        if (donutNeedsToStop)
+        {
+
+        }
+    }
+
+    void GetInput()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+    }
+
+    void MoveSideways()
+    {
+        this.transform.Translate(new Vector3(horizontalMovement,0,0));
+    }
+
+
+
+    // Update is called once per frame
+    void Update ()
 	{
 		SpinSpeed.x = (Time.deltaTime * 225f);
-		
+        donutNeedsToStop = this.transform.position.x > leftConstraint || this.transform.position.x < rightConstraint;
+        GetInput();
+        MoveSideways();
 		this.transform.Rotate(SpinSpeed);
 	}
+
+
 }
---
+++ DonutGit/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //The speed at which the player travels.
+    public Vector3 speed;
+
+    [Range(3, 5)]
+    public int pathways_locked = 3;
+    [Range(0, 5)]
+    public float pathways;
+
+    const float
+        x = 1,
+        y = 1,
+        z = 1;
+    private readonly Vector3 k_defaultSpeed = new Vector3(
+        x,
+        y,
+        z
+        );
+
+    Gyroscope m_gyro;
+     
+    enum CharacterHorizontalMovement
+    {
+        Gyroscope,
+        Touchscreen,
+        Arrowkeys,
+        Defunct
+    };
+
+    CharacterHorizontalMovement CHM;
+
+    //public Vector3 Speed();
+   
+    // Use this for initialization
+    void Start()
+    {
+        m_gyro = Input.gyro;
+        //Enum to define what will control the character 
+        InitialiseInput();
+    }
+
+    void SetSpeedToDefault()
+    {
+        //This method resets speed to default. To be used after a speedboost.
+        speed = k_defaultSpeed;
+    }
+
+    void SpeedUp(float speedMultiplier)
+    {
+        //This method speeds up the player as they pass over a jump-pad; but can be reused
+        speed *= speedMultiplier;
+    }
+
+    //This method speeds up the player for a specified amount of time, before returning the speed to default
+    void SpeedUpTemporarily(float speedMultiplier)
+    {
+        SpeedUp(speedMultiplier);
+        //DoSomethingWithTime()
+
+    }
+    
+    //Move the player avatar left or right, but they can only be in one of 3 to 5 places.
+    void MoveSidewaysLocked(int pathways)
+    {
+        
+    }
+
+    //Move the player avatar left or right, using the gyroscope.
+    void MoveSideWays()
+    {
+
+    }
+
+    //This method sets up the the game's input
+    void InitialiseInput()
+    {
+        //Is gyroscope enabled?
+        if (m_gyro.enabled) //https://docs.unity3d.com/ScriptReference/Input-gyro.html
+        {
+            CHM = CharacterHorizontalMovement.Gyroscope;
+        }
+        //Does touchscreen work? This will let us use the touchscreen instead
+        else if (SystemInfo.deviceType == DeviceType.Handheld)  //https://docs.unity3d.com/ScriptReference/SystemInfo.html
+        {
+            CHM = CharacterHorizontalMovement.Touchscreen;
+        }
+        //Are we using a desktop (thus needing a keyboard)?
+        else if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            CHM = CharacterHorizontalMovement.Arrowkeys;
+            //Initialise arrow keys
+        }
+        //Nothing works; so display an error message.
+        else
+        {
+            CHM = CharacterHorizontalMovement.Defunct;
+        }
+    }
+
+    void GetGyroscopicInput()
+    {
+        //if (Input.gyro.)
+    }
+
+
+
+    void GetInput()
+    {
+        if (CHM == CharacterHorizontalMovement.Gyroscope)
+        {
+
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        //Use this for input
+        GetInput();
+    }
+}
