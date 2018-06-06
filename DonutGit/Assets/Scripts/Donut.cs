7b5903f20d55f2b3d48d2b5c4b22a1c4e5e43a5c
Jibzi <george.armstrong@mail.bcu.ac.uk>
2018-06-06 17:18:23 +0100

    Basic Movement Input

--- DonutGit/Assets/Scripts/Donut.cs
+++ DonutGit/Assets/Scripts/Donut.cs
@@ -4,52 +4,19 @@ using UnityEngine;
 
 public class Donut : MonoBehaviour
 {
-    private float horizontalMovement;
     private Vector3 SpinSpeed;
-    //The points between which the donut's left-to-right movement is constrained
-    public float 
-        leftConstraint,
-        rightConstraint;
-    private Vector2 HorizontalConstraints;
-    private bool hasMovedTooFarLeft;
-    private bool hasMovedTooFarRight;
-    [SerializeField]
-    float sidewaysSpeed;
+
+	[SerializeField] private float MoveSpeed;
+
+	private float leftConstraint = -5f;
+	private float rightConstraint = 5f;
 
     // Use this for initialization
     void Start()
     {
-        leftConstraint = -5;
-        rightConstraint = 5;
-        HorizontalConstraints = new Vector2(leftConstraint, rightConstraint);
-        horizontalMovement = Input.GetAxis("Horizontal");
-        hasMovedTooFarLeft = this.transform.position.x < leftConstraint;
-        hasMovedTooFarRight = this.transform.position.x > rightConstraint;
+
     }
 
-    void ClampHorizontalMovement()
-    {
-        //What happens if it goes too far left?
-        if (!hasMovedTooFarLeft)
-        {
-            
-        }
-        //What happens if it goes too far right?
-        if (!hasMovedTooFarRight)
-        {
-            
-        }
-    }
-
-    void GetInput()
-    {
-        horizontalMovement = Input.GetAxis("Horizontal");
-    }
-
-    void MoveSideways()
-    {
-        this.transform.Translate(new Vector3(horizontalMovement*Time.deltaTime*sidewaysSpeed,0,0));
-    }
 
 
 
@@ -59,12 +26,32 @@ public class Donut : MonoBehaviour
 
         SpinSpeed.x = (Time.deltaTime * 225f); //225f
 
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			if (transform.position.x < leftConstraint)
+			{
+				transform.position.Set(leftConstraint, transform.position.y, transform.position.z);
+			}
+			else
+			{
+				this.transform.Translate(-MoveSpeed * Time.deltaTime, 0,0);
+			}
 
+		}
 
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			if (transform.position.x > rightConstraint)
+			{
+				transform.position.Set(rightConstraint, transform.position.y, transform.position.z);
+			}
+			else
+			{
+				this.transform.Translate(MoveSpeed * Time.deltaTime, 0,0);
+			}
 
-        GetInput();
-        
-        MoveSideways();
+		}
+     
 		this.transform.Rotate(SpinSpeed);
 	}
 
