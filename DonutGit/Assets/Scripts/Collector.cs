77ab09cb72f070ef1e6bcc09d955f7f7e36f4c14
BrujoCervino <39952594+BrujoCervino@users.noreply.github.com>
2018-06-07 15:16:14 +0100

    Added coin prefab, began working on score and collectory events

---
+++ DonutGit/Assets/Scripts/Collector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour {
+
+    GameObject Collectable;
+
+    [SerializeField] SphereCollider SC;
+    [SerializeField] private float timeToDespawn;
+    private float collectablesCollected;
+
+    // Use this for initialization
+    void Start()
+    {
+        Collectable = GameObject.FindWithTag("Collectable");
+    }
+
+    void AddToCollectablesCollected()
+    {
+        //Increment the number of collectables collected, which will influence the score.
+        collectablesCollected++;
+    }
+
+    
+
+    void ResetCollectable()
+    {
+        Collectable = new GameObject();
+    }
+
+    void DespawnCollectable()
+    {
+        //Remove the coin from the scene after it has passed the camera.
+        Destroy(Collectable, timeToDespawn); //https://docs.unity3d.com/ScriptReference/Object.Destroy.html
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
