2611cf387d3afdda604a74733009a3c545372320
Jibzi <george.armstrong@mail.bcu.ac.uk>
2018-06-20 15:20:58 +0100

    Revert "Misc"
    This reverts commit 5c2684da67fa363b1215be35cf1d0eb29c4c3d27.

--- DonutGit/Assets/Scripts/Kieran/ChromaticRandomiser.cs
+++ DonutGit/Assets/Scripts/Kieran/ChromaticRandomiser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,8 @@ public class ChromaticRandomiser : MonoBehaviour {
     [Tooltip("The textures to be randomly applied to the material.")]
     //Array of textures. Indices are integral, rather than in the form of bytes, to maintain 32-bit memory size.
     public Texture[] textures;
+    System.Random rand = new System.Random();
+    
     private int indexOfLastUsedElement;
 
     void RandomiseColour()
@@ -18,7 +21,6 @@ public class ChromaticRandomiser : MonoBehaviour {
         Dictionary<int, Color> colours = new Dictionary<int, Color>();
         colours.Add(0, Color.green);
         colours.Add(1, Color.red);
-<<<<<<< HEAD
         //The key of the to be allocated colour
         int chromaticKey;
         //Max reachable key of dictionary (in accordance with zero-based indexing)
@@ -38,16 +40,6 @@ public class ChromaticRandomiser : MonoBehaviour {
         {
             Debug.Log(@"chromaticKey value was {chromaticKey}");
         }   
-=======
-        //The index of the to be allocated colour
-        int chromaticIndex;
-        //The number by which we will modulo delta time. This must be equal to the array's length - 1, accounting for the extra number 0.
-        int divisor = colours.Count + 1;// - 1;
-        //The index of the colour. We need it to be an int, in accordance with that an array's index must be integral.
-        chromaticIndex = (int)Mathf.RoundToInt(Time.time % divisor);
-        _material.color = colours[chromaticIndex];
-        Debug.Log(colours[chromaticIndex]);
->>>>>>> 61440514a0a8fde86255da381cb62adc0efcf27b
     }
 
     void RandomiseTexture()
