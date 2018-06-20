61440514a0a8fde86255da381cb62adc0efcf27b
Jibzi <george.armstrong@mail.bcu.ac.uk>
2018-06-20 14:20:46 +0100

    Ramping world speed

--- DonutGit/Assets/Scripts/George/RoadPartComponent.cs
+++ DonutGit/Assets/Scripts/George/RoadPartComponent.cs
@@ -14,12 +14,14 @@ public class RoadPartComponent : MonoBehaviour
 {
 
 	private PopulateRoad _populateRoad;
+	private WorldMover _worldMover;
 	
 	// Use this for initialization
 	void Start ()
 	{
 
 		_populateRoad = gameObject.GetComponentInParent<PopulateRoad>();
+		_worldMover = gameObject.GetComponentInParent<WorldMover>();
 	}
 
 	
@@ -27,7 +29,7 @@ public class RoadPartComponent : MonoBehaviour
 	void Update ()
 	{
 //TODO: Make this speed a variable so that we can get faster and slower during gameplay.
-		this.transform.Translate(0,0, -(Time.deltaTime * 10));
+		this.transform.Translate(0,0, (-_worldMover.WorldSpeed * Time.deltaTime));
 
 		//Teleport any road parts that are behind the camera to the far end of the road.
 		if (this.transform.position.z < -20f)
---
+++ DonutGit/Assets/WorldMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMover : MonoBehaviour
+{
+
+	//
+	//Author: George
+	//
+	//Manager script that handles the world speed variable that the road peices can grab, so they can slowly ramp up in speed.
+	//
+
+	public float WorldSpeed;
+
+	[SerializeField]
+	[Range(0f, 1f)]
+	[Tooltip(
+		"How much extra speed is added to the world speed every second. Recommended 0.1")]
+	private float _worldAcceleration;
+
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+
+		WorldSpeed = 10f;
+	}
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+			WorldSpeed += (Time.deltaTime * _worldAcceleration);
+	}
+}
