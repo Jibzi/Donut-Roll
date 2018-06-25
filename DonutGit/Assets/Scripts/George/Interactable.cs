cccd3d358b6c5acf0fbd14d8b8b1ec3c51a55f17
flyscript <willchap6@gmail.com>
2018-06-25 13:31:14 +0100

    Have fun dying

--- DonutGit/Assets/Scripts/George/Interactable.cs
+++ DonutGit/Assets/Scripts/George/Interactable.cs
@@ -7,13 +7,21 @@ public class Interactable : MonoBehaviour
 
     //
     //Author: George
+    //        Will
     //
     //Purely to set the structor of children that inherit from this, so that unique scripts can be called all through 
     //this one virtual function.
     //
+    private bool canCollide = true;
     
     public virtual void Interact(Donut interactor)
     {
         
+    }
+
+    public bool CanCollide
+    {
+        get { return canCollide; }
+        set { canCollide = value; }
     }
 }
